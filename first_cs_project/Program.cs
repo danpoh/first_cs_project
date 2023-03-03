16837ebb19efb1acbf8458440b08bdc02fcbf845
Daniel Oh <daniel0625z@gmail.com>
2023-03-04 00:01:51 +0900

    Commit test

--- first_cs_project/Program.cs
+++ first_cs_project/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* A dialogue function */
+Console.WriteLine("What kind of thing are we talking about?");
+string a = Console.ReadLine();//this is the talking point
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+string b = Console.ReadLine();//this adds another adjective to the pile
+string c = " of Doom";//the origin of DOOMMMM
+string d = "3000";//to exaggerate 
+Console.WriteLine("The " + b + " " + a + c + d + "!");
