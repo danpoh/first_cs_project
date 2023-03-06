cd5a2b0d9aa43ad9934e5dd8d1d2730b312f0fdd
Daniel Oh <daniel0625z@gmail.com>
2023-03-07 01:44:02 +0900

    07/03/23

--- first_cs_project/Program.cs
+++ first_cs_project/Program.cs
@@ -1,8 +1,44 @@
-﻿/* A dialogue function */
-Console.WriteLine("What kind of thing are we talking about?");
-string a = Console.ReadLine();//this is the talking point
-Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
-string b = Console.ReadLine();//this adds another adjective to the pile
-string c = " of Doom";//the origin of DOOMMMM
-string d = "3000";//to exaggerate 
-Console.WriteLine("The " + b + " " + a + c + d + "!");
+﻿Console.WriteLine("Input a int number: ");
+int intnumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a float number: ");
+float floatnumber = (float)Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Input a decimal number: ");
+decimal decimalnumber = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Input a double number: ");
+double doublenumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a short number: ");
+short shortnumber = (short)Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Input a ushort number: ");
+ushort ushortnumber = (ushort)Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a longnumber number: ");
+long longnumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a ulong number: ");
+ulong ulongnumber = (ulong)Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a uint number: ");
+uint uintnumber = (uint)Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a byte number: ");
+byte bytenumber = (byte)Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a sbyte number: ");
+sbyte sbytenumber = (sbyte)Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a character: ");
+char charnumber = Convert.ToChar(Console.ReadLine()!);
+Console.WriteLine("Input a text: ");
+string stringnumber = Console.ReadLine()!;
+Console.WriteLine("True or False: ");
+bool boolnumber = Convert.ToBoolean(Console.ReadLine());
+
+
+
+Console.WriteLine("A float number: " + floatnumber);
+Console.WriteLine("A decimal number: " + decimalnumber);
+Console.WriteLine("A double number: " + doublenumber);
+Console.WriteLine("A short number: " + shortnumber);
+Console.WriteLine("A ushort number: " + ushortnumber);
+Console.WriteLine("A long number: " + longnumber);
+Console.WriteLine("A ulong number: " + ulongnumber);
+Console.WriteLine("A uint number: " + uintnumber);
+Console.WriteLine("A byte number: " + bytenumber);
+Console.WriteLine("A sbyte number:" + sbytenumber);
+Console.WriteLine("A single character: " + charnumber);
+Console.WriteLine("A string: " + stringnumber);
+Console.WriteLine("True or false?: " + boolnumber);
