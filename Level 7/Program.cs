5a12aa925ddb0edf31054beb01fef17edbb97511
Daniel Oh <96776182+danpoh@users.noreply.github.com>
2023-03-17 23:16:30 +0900

    Update Program.cs
    A video game that coordinates a protective shield onto a square that is on a 8x8 board.

--- Level 7/Program.cs
+++ Level 7/Program.cs
@@ -1,9 +1,11 @@
-﻿int x;
-
-x = 5;
-int y = x++;
-Console.WriteLine(y);
-
-x = 5;
-int z = x++;
-Console.WriteLine(z);  
+Console.Title = "Defense of Consolas";
+Console.Write("Target Row? ");
+int targetRow = Convert.ToInt32(Console.ReadLine());
+Console.Write("Target Column? ");
+int targetColumn = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Deploy to");
+Console.WriteLine($"({targetRow + 1}, {targetColumn})");
+Console.WriteLine($"({targetRow}, {targetColumn + 1})");
+Console.WriteLine($"({targetRow}, {targetColumn - 1})");
+Console.WriteLine($"({targetRow - 1}, {targetColumn})");
+Console.Beep();
